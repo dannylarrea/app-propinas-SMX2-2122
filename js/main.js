fc2d464ebffd8c31579580410bef463582cee2e2
dannylarrea <danny.larrea.argudo@gmail.com>
2022-04-28 18:25:32 +0200

    final

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 function calcProp() {
-    
+    var total=document.getElementById('total').value
+    var opciones=document.getElementById('servicio').value
+    var personas=document.getElementById('personas').value
+    var resultado=Number(total)*Number(opciones)/Number(personas)
+    var result=document.getElementById('respuesta')
+    result.innerHTML="La propina es: "+resultado
 }
